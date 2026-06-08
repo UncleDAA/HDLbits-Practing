4989323084477fcf9d1eb06835db90abbc144207
UncleDAA <danny.seer@gmail.com>
2026-06-09 01:47:51 +0800

    Update 2.5.7.Bcdadd100.v

--- 2.Verilog Language/2.5.More Verilog Feature/2.5.7.Bcdadd100.v
+++ 2.Verilog Language/2.5.More Verilog Feature/2.5.7.Bcdadd100.v
@@ -1,1 +1,20 @@
-
+module top_module( 
+    input [399:0] a, b,
+    input cin,
+    output cout,
+    output [399:0] sum );
+    wire [99:0] carry;
+    bcd_fadd(a[3:0],b[3:0],cin,carry[3:0],sum[3:0]);
+    genvar i;
+    generate
+        for(i=1;i<100;i++) begin:bcd_add
+            bcd_fadd(
+                .a(a[4*i+3:4*i]),
+                .b(b[4*i+3:4*i]),
+                .cin(carry[i-1]),
+                .cout(carry[i]),
+                .sum(sum[4*i+3:4*i]));
+        end
+    endgenerate
+    assign cout=carry[99];
+endmodule
