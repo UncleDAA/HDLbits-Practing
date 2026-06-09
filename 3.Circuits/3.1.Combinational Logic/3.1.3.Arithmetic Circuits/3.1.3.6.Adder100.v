a06c0fb3ade2612a62a3904a6b4d2990fbccb0ce
UncleDAA <danny.seer@gmail.com>
2026-06-09 16:56:10 +0800

    Update 3.1.3.6.Adder100.v

--- 3.Circuits/3.1.Combinational Logic/3.1.3.Arithmetic Circuits/3.1.3.6.Adder100.v
+++ 3.Circuits/3.1.Combinational Logic/3.1.3.Arithmetic Circuits/3.1.3.6.Adder100.v
@@ -25,3 +25,14 @@ module fadd(
     assign cout=((a|b)&cin)|a&b;
 
 endmodule
+
+// REF //
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output cout,
+    output [99:0] sum );
+    
+    assign {cout,sum} = a+b+cin;
+
+endmodule
