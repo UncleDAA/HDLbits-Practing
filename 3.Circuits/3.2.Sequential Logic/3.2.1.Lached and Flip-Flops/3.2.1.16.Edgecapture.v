3666aa74a3ee25a645656c5cfa9335c7e05e0a4d
UncleDAA <danny.seer@gmail.com>
2026-06-11 18:39:14 +0800

    Create 3.2.1.16.Edgecapture.v

---
+++ 3.Circuits/3.2.Sequential Logic/3.2.1.Lached and Flip-Flops/3.2.1.16.Edgecapture.v
@@ -0,0 +1,22 @@
+module top_module (
+    input clk,
+    input reset,
+    input [31:0] in,
+    output [31:0] out
+);
+    reg [31:0] last_in;
+    reg [31:0] edged;
+    always @(posedge clk) begin
+        for(int i=0;i<32;i++) begin
+            if(~in[i] & last_in[i] & ~edged[i]) begin
+                edged[i]<=1;
+            end
+        end
+        if(reset) begin
+            edged <= 32'b0;   
+        end        
+        last_in <= in;
+    end
+    assign out=edged;
+endmodule
+// 有趣的事情是因為reset的優先級更高 所以應該最後確認更新
