a3a10d8297acc9b92c455c2b9a384252f8430d1f
UncleDAA <danny.seer@gmail.com>
2026-06-11 13:57:35 +0800

    Update 3.2.1.11.Mt2015 muxdff.v

--- 3.Circuits/3.2.Sequential Logic/3.2.1.Lached and Flip-Flops/3.2.1.11.Mt2015 muxdff.v
+++ 3.Circuits/3.2.Sequential Logic/3.2.1.Lached and Flip-Flops/3.2.1.11.Mt2015 muxdff.v
@@ -6,8 +6,8 @@ module top_module (
 	output reg Q);
     always @(posedge clk) begin
         if(L)
-            Q=r_in;
+            Q<=r_in;
         else
-            Q=q_in;
+            Q<=q_in;
     end
 endmodule
