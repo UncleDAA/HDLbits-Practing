25b47eed7e46110191ef5005e75ac1b1c7d3e84f
UncleDAA <danny.seer@gmail.com>
2026-06-12 19:20:58 +0800

    Create 3.2.2.4.Countslow.v

---
+++ 3.Circuits/3.2.Sequential Logic/3.2.2.Counters/3.2.2.4.Countslow.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    always @(posedge clk) begin
+        if(slowena) begin
+            if(q == 4'd9)
+                q<=4'b0;
+            else
+                q<=q+1;
+        end
+        if(reset)
+            q<=4'b0;
+    end
+        
+        
+endmodule
