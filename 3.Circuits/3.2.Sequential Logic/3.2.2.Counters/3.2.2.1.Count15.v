ff55435b5668daca8f8c97521f6f12b6a7a7b5f6
UncleDAA <danny.seer@gmail.com>
2026-06-12 17:36:37 +0800

    Update 3.2.2.1.Count15.v

--- 3.Circuits/3.2.Sequential Logic/3.2.2.Counters/3.2.2.1.Count15.v
+++ 3.Circuits/3.2.Sequential Logic/3.2.2.Counters/3.2.2.1.Count15.v
@@ -1,1 +1,13 @@
-
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output [3:0] q);
+    reg [3:0] current = 4'b0;
+    always @(posedge clk) begin
+        if(reset)
+            current=0;
+    	else
+            current+=1;
+    end
+    assign q=current;
+endmodule
