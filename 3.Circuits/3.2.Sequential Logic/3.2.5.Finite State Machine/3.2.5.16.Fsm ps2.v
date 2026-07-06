921ff333383736c10b1c5f6aa2e5607bf95ec812
UncleDAA <danny.seer@gmail.com>
2026-07-06 21:54:13 +0800

    Update 3.2.5.16.Fsm ps2.v

--- 3.Circuits/3.2.Sequential Logic/3.2.5.Finite State Machine/3.2.5.16.Fsm ps2.v
+++ 3.Circuits/3.2.Sequential Logic/3.2.5.Finite State Machine/3.2.5.16.Fsm ps2.v
@@ -5,30 +5,30 @@ module top_module(
     output done
 ); //
     reg [1:0] state, next;
-	parameter empty=2'b00,bit1=2'b01,bit2=2'b10,bit3=2'b11;
+	parameter bit1=2'b00,bit2=2'b01,bit3=2'b10,out=2'b11;
     // State transition logic (combinational)
     always @(*) begin
         case(state)
-            empty: begin
-                if(in[3]) next=bit1;
-                else next=empty;
+            bit1: begin
+                if(in[3]) next=bit2;
+                else next=bit1;
             end
-            bit1:next=bit2;
             bit2:next=bit3;
-            bit3: begin
-                if(in[3]) next=bit1;
-                else next=empty;
+            bit3:next=out;
+            out: begin
+                if(in[3]) next=bit2;
+                else next=bit1;
             end
         endcase
     end
                    
     // State flip-flops (sequential)
     always @(posedge clk) begin
-        if(reset) state<=empty;
+        if(reset) state<=bit1;
         else state<=next;
     end
             
     // Output logic
-    assign done=(state==bit3);
-
+    assign done=(state==out);
+    
 endmodule
