9b47d100f7566a2dad03ddfc1d3d6f2e0908389d
UncleDAA <danny.seer@gmail.com>
2026-07-06 22:04:03 +0800

    Update 3.2.5.16.Fsm ps2data.v

--- 3.Circuits/3.2.Sequential Logic/3.2.5.Finite State Machine/3.2.5.16.Fsm ps2data.v
+++ 3.Circuits/3.2.Sequential Logic/3.2.5.Finite State Machine/3.2.5.16.Fsm ps2data.v
@@ -2,33 +2,49 @@ module top_module(
     input clk,
     input [7:0] in,
     input reset,    // Synchronous reset
-    output done
-); //
+    output [23:0] out_bytes,
+    output done); //
+    // FSM from fsm_ps2  
     reg [1:0] state, next;
-	parameter bit1=2'b00,bit2=2'b01,bit3=2'b10,out=2'b11;
-    // State transition logic (combinational)
+    reg [23:0] out_buffer;
+    parameter bit1=2'b00,bit2=2'b01,bit3=2'b10,out=2'b11;
     always @(*) begin
         case(state)
             bit1: begin
                 if(in[3]) next=bit2;
                 else next=bit1;
             end
-            bit2:next=bit3;
-            bit3:next=out;
+            bit2: begin
+                next=bit3;
+            end
+            bit3: begin
+                next=out;
+            end
             out: begin
                 if(in[3]) next=bit2;
                 else next=bit1;
             end
         endcase
     end
-                   
-    // State flip-flops (sequential)
     always @(posedge clk) begin
-        if(reset) state<=bit1;
-        else state<=next;
+        if(reset) begin
+            state<=bit1;
+        end
+        else begin
+            state<=next;
+            case(state)
+                bit1:out_buffer[23:16]<=in;
+                bit2:out_buffer[15:8]<=in;
+                bit3:out_buffer[7:0]<=in;
+                out: begin
+                    if(in[3]) out_buffer[23:16]<=in;
+                end
+            endcase
+        end
     end
-            
-    // Output logic
-    assign done=(state==out);
     
+    assign done=(state==out);
+    // New: Datapath to store incoming bytes.
+    assign out_bytes=(state==out?out_buffer:24'b0);
+
 endmodule
