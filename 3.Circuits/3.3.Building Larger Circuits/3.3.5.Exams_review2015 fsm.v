c946e09d0b2fb0ad0aa3f3628615ed60afb1a509
UncleDAA <danny.seer@gmail.com>
2026-07-16 00:47:21 +0800

    Update 3.3.5.Exams_review2015 fsm.v

--- 3.Circuits/3.3.Building Larger Circuits/3.3.5.Exams_review2015 fsm.v
+++ 3.Circuits/3.3.Building Larger Circuits/3.3.5.Exams_review2015 fsm.v
@@ -28,3 +28,60 @@ module top_module (
     assign start_shifting = (state == SHIFT) ;
             
 endmodule
+
+// REF //
+module top_module (
+    input clk,
+    input reset,      // Synchronous reset
+    input data,
+    output shift_ena,
+    output counting,
+    input done_counting,
+    output done,
+    input ack );
+    parameter SEARCH_1=0,SEARCH_11=1,SEARCH_110=2,SEARCH_1101=3,SHIFT_1=4,SHIFT_2=5,SHIFT_3=6,SHIFT_4=7,COUNTING=8,DONE=9;
+    reg [3:0]state,next;
+    
+    always @(*) begin
+        case(state)
+            SEARCH_1: begin
+                if(data) next=SEARCH_11;
+                else next=SEARCH_1;
+            end
+            SEARCH_11: begin
+                if(data) next=SEARCH_110;
+                else next=SEARCH_1;
+            end
+            SEARCH_110: begin
+                if(!data) next=SEARCH_1101;
+                else next=SEARCH_110;
+            end
+            SEARCH_1101: begin
+                if(data) next=SHIFT_1;
+                else next=SEARCH_1;
+            end
+            SHIFT_1: next=SHIFT_2;
+            SHIFT_2: next=SHIFT_3;
+            SHIFT_3: next=SHIFT_4;
+            SHIFT_4: next=COUNTING;
+            COUNTING: begin
+                if(!done_counting) next=COUNTING;
+                else next=DONE;
+            end
+            DONE: begin
+                if(ack) next=SEARCH_1;
+                else next=DONE;
+            end
+        endcase
+    end
+    always @(posedge clk) begin
+        if(reset) state<=SEARCH_1;
+        else state<=next;
+    end
+    
+    assign shift_ena = (state == SHIFT_1 || state == SHIFT_2 ||state == SHIFT_3 ||state == SHIFT_4) ;
+    assign counting = (state == COUNTING);
+    assign done = (state == DONE);
+                
+
+endmodule
