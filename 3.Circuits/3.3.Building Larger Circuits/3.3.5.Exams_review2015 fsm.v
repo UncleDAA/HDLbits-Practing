712c107c54dad79d0d3d9d984a99e339d3c0b883
UncleDAA <danny.seer@gmail.com>
2026-07-16 00:18:01 +0800

    Update 3.3.5.Exams_review2015 fsm.v

--- 3.Circuits/3.3.Building Larger Circuits/3.3.5.Exams_review2015 fsm.v
+++ 3.Circuits/3.3.Building Larger Circuits/3.3.5.Exams_review2015 fsm.v
@@ -1,1 +1,30 @@
-
+module top_module (
+    input clk,
+    input reset,      // Synchronous reset
+    input data,
+    output start_shifting);
+	parameter SEARCH=0,SHIFT=1;
+    reg state,next;
+    reg [3:0]buffer;
+    always @(*) begin
+        case(state)
+            SEARCH: begin
+                if({buffer[2:0],data}==4'b1101) next=SHIFT;
+                else next=SEARCH;
+            end
+            SHIFT: next=SHIFT;
+        endcase
+    end
+    always @(posedge clk) begin
+        if(reset) begin
+            state<=SEARCH;
+            buffer<=0;
+        end
+        else begin
+            state<=next;
+            if(state==SEARCH) buffer<={buffer[2:0],data};
+        end
+    end
+    assign start_shifting = (state == SHIFT) ;
+            
+endmodule
