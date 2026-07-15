5985f2391a4a8f6aedd9a86e4952b433702705d4
UncleDAA <danny.seer@gmail.com>
2026-07-16 00:58:33 +0800

    Update 3.3.5.Exams_review2015 fsm.v

--- 3.Circuits/3.3.Building Larger Circuits/3.3.5.Exams_review2015 fsm.v
+++ 3.Circuits/3.3.Building Larger Circuits/3.3.5.Exams_review2015 fsm.v
@@ -2,31 +2,58 @@ module top_module (
     input clk,
     input reset,      // Synchronous reset
     input data,
-    output start_shifting);
-	parameter SEARCH=0,SHIFT=1;
-    reg state,next;
-    reg [3:0]buffer;
+    output shift_ena,
+    output counting,
+    input done_counting,
+    output done,
+    input ack );
+    parameter SEARCH=0,SHIFT=1,COUNTING=2,DONE=3;
+    reg [3:0]state,next;
+    reg [3:0]search_buffer;
+    reg [1:0]counter;
+    
     always @(*) begin
         case(state)
             SEARCH: begin
-                if({buffer[2:0],data}==4'b1101) next=SHIFT;
+                if({search_buffer[2:0],data}==4'b1101) next=SHIFT;
                 else next=SEARCH;
             end
-            SHIFT: next=SHIFT;
+   			SHIFT: begin
+                if(counter==2'b11) next=COUNTING;
+                else next=SHIFT;
+            end
+            COUNTING: begin
+                if(!done_counting) next=COUNTING;
+                else next=DONE;
+            end
+            DONE: begin
+                if(ack) next=SEARCH;
+                else next=DONE;
+            end
         endcase
     end
     always @(posedge clk) begin
         if(reset) begin
             state<=SEARCH;
-            buffer<=0;
+            search_buffer<=0;
+            counter<=0;
         end
         else begin
             state<=next;
-            if(state==SEARCH) buffer<={buffer[2:0],data};
+            case(state)
+                SEARCH: search_buffer<={search_buffer[2:0],data};
+                SHIFT: counter<=counter+1;
+                DONE: begin
+                    search_buffer<=0;
+                    counter<=0;
+                end
+            endcase
         end
     end
-    assign start_shifting = (state == SHIFT) ;
-            
+    
+    assign shift_ena = (state == SHIFT) ;
+    assign counting = (state == COUNTING);
+	assign done = (state == DONE);
 endmodule
 
 // REF //
